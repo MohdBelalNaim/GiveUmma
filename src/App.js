f9f8f55499db0bea215456babf1542c52169a9fb
urooj khan <uroojk844@gmail.comm>
2024-01-13 13:32:25 +0530

    added details page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import HomeNavbar from './components/HomeNavbar';
+import DetailsPage from './pages/DetailsPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/details" element={<DetailsPage/>} />
       </Routes>
       <Footer/>
     </BrowserRouter>
