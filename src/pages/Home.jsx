569a608933ab396d2ef41a5314d78a563de1df93
urooj khan <uroojk844@gmail.comm>
2024-01-16 14:15:28 +0530

    added donation form and model component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styles from '../assets/css/home.module.css'
 import HomeNavbar from '../components/HomeNavbar'
 import CampaignCard from '../components/CampaignCard'
